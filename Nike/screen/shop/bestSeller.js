9030350f14d568cabcb6213e110b0071d2952e1b
Sơn Nguyễn <20106421.son@student.iuh.edu.vn>
2023-11-23 02:42:28 +0700

    screen shop part6- create navigation subscreen highlight

---
+++ Nike/screen/shop/bestSeller.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const bestSeller = () => {
+  return (
+    <View>
+      <Text>bestSeller</Text>
+    </View>
+  )
+}
+
+export default bestSeller
