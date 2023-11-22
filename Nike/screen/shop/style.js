4b070b104d7e3f06e8697e96de27df5a14aa29c2
Sơn Nguyễn <20106421.son@student.iuh.edu.vn>
2023-11-22 16:43:33 +0700

    screen shop part 1

---
+++ Nike/screen/shop/style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    img:{
+        width: 100,
+        height: 100,
+        borderRadius: 10
+    }
+})
+
+export default styles;
