0b1dd017df931ba5cf1e26f74b52835b2015d5cb
Sơn Nguyễn <20106421.son@student.iuh.edu.vn>
2023-11-26 16:57:22 +0700

    finish Address Screen. Fix some bug

--- Nike/App.js
+++ Nike/App.js
@@ -1,8 +1,8 @@
 import { Provider } from "react-redux";
-// import Auth from './screen/user/user.js'
+import Auth from './screen/user/user.js'
 import store from './redux-toolkit/store.js'
 // import Auth from './screen/checkout/checkout.js'
-import Auth from './screen/Auth/Auth.js'
+// import Auth from './screen/Auth/Auth.js'
 
 export default function app() {
     return(
