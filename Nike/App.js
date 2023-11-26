0971226ddf970504192fb8362d260688104dd092
Sơn Nguyễn <20106421.son@student.iuh.edu.vn>
2023-11-26 15:21:04 +0700

    create Address Component, create Modal Component, update User Screen, allow change phone and email of User

--- Nike/App.js
+++ Nike/App.js
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 // import Auth from "./screen/Auth/Auth.js";
 import Auth from './screen/user/user.js'
+// import Auth from './screen/address/address.js'
 import store from './redux-toolkit/store.js'
 // import Auth from "./screen/sign_in/sign_in";
 // import Auth from './screen/shop/shopHome'
